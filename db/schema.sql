b520678165d36a24713224fb9cc14762d1171717
doulos tou Christou <jeremyhancock32@gmail.com>
2018-11-09 12:28:51 -0500

    all function, no style

--- db/schema.sql
+++ db/schema.sql
@@ -8,6 +8,6 @@ CREATE TABLE burgers
 id int NOT NULL AUTO_INCREMENT,
 burger_name varchar(255) NOT NULL,
 burger_description varchar (255) NOT NULL,
-devoured boolean NOT NULL,
+devoured BOOLEAN DEFAULT false,
 PRIMARY KEY (id)
 );
